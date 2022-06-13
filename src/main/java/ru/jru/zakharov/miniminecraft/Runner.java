b737fe495d9a72d09e57d3f89c8b2d0589af08cf
SergeiDevelp <ya.z-serg@yandex.ru>
2022-06-13 16:37:23 +0900

    test new branch

--- src/main/java/ru/jru/zakharov/miniminecraft/Runner.java
+++ src/main/java/ru/jru/zakharov/miniminecraft/Runner.java
@@ -4,5 +4,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello Maven and Minecraft");
         System.out.println("Hello Git");
+        System.out.println("Hello branch Dev_1");
     }
 }
