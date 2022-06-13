1a4247f466de495933132ef9cf36e17fc3e2f3ec
SergeiDevelp <ya.z-serg@yandex.ru>
2022-06-13 16:30:13 +0900

    test

--- src/main/java/ru/jru/zakharov/miniminecraft/Runner.java
+++ src/main/java/ru/jru/zakharov/miniminecraft/Runner.java
@@ -3,5 +3,6 @@ package ru.jru.zakharov.miniminecraft;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello Maven and Minecraft");
+        System.out.println("Hello Git");
     }
 }
